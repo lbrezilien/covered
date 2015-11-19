b5211805713023f154dbb64c0c6f47ca9fc5a9ed
mbukantz <mbukantz@gmail.com>
2015-11-19 18:46:12 -0500

    removed extraneous code in tab bar

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -6,16 +6,4 @@ class AppDelegate < PM::Delegate
     open_tab_bar HomeScreen, Search, MyList, LogOut
   end
 
-  def build_tabbar
-    tabbar = UITabBarController.alloc.init
-    tabbar.viewControllers = [
-      UINavigationController.alloc.initWithRootViewController(SearchController.alloc.init),
-      UINavigationController.alloc.initWithRootViewController(MyListController.alloc.init),
-      UINavigationController.alloc.initWithRootViewController(LogOutController.alloc.init)
-    ]
-    tabbar.selectedIndex = 0
-    tabbar.delegate = self
-    tabbar
-  end
-
 end
